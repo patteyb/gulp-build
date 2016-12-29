61e382564687c5f874870a1ce9859427c19af9db
patteyb <patteybleecker@gmail.com>
2016-12-29 10:53:20 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+"use strict";
+
+var gulp    = require('gulp'),
+  imagemin  = require('gulp-imagemin'),
+   flatten  = require('gulp-flatten'),
+     uglify = require('gulp-uglify'),
+     eslint = require('gulp-eslint'),
+     concat = require('gulp-concat'),
+     rename = require('gulp-rename'),
+     useref = require('gulp-useref'),
+       sass = require('gulp-sass'),
+       maps = require('gulp-sourcemaps'),
+        del = require('del'),
+        iff = require('gulp-if'),
+       csso = require('gulp-csso');
+
+     var paths = {
+       scripts: './js/**/*',
+       sass: 'sass/**/*.scss',
+       html: 'index.html',
+       images: './images/**/*',
+       icons: '/icons/**',
+       css: '/css/*.css'
+     };
+
+     var dist = {
+       base: 'dist',
+       styles: './dist/styles',
+       scripts: './dist/scripts',
+       icons: './dist/icons',
+       content: './dist/content'
+     };
+
+/** CLEAN TASKS ------------------------  */
+/** Delete the dist directory, js files, and css directory */
+gulp.task('clean', function() {
+  return del.sync(['./dist', 'js/*.*', 'css/']);
+});
+
+/** Delete directories made when compiling and minifying styles */
+gulp.task('cleanStyles', function() {
+  return del.sync([dist.styles, 'css']);
+});
+
+/** Delete directories made when concatenating and minifying scripts */
+gulp.task('cleanScripts', function() {
+  return del.sync(['js/*.*', dist.scripts]);
+});
+
+/** STYLE MANIPULATION ------------------  */
+/** Compile sass files and make sourcemaps */
+gulp.task('compileSass', function() {
+  gulp.src(paths.sass)
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(flatten())
+      .pipe(gulp.dest('css'));
+  del.sync('css/circle');
+});
+
+/** Rename style files to reflect impending minification */
+gulp.task('renameStyles', ['compileSass'], function() {
+    return gulp.src(paths.css)                          
+      .pipe(rename('all.min.css'))
+      .pipe(gulp.dest('./dist/styles'));
+});
+
+/** Minify styles and store in final destination */
+gulp.task('styles', ['cleanStyles', 'renameStyles'], function() {
+    return gulp.src(dist.styles + '/*.css')                          
+      .pipe(csso())
+      .pipe(gulp.dest(dist.styles));
+});
+
+/** SCRIPT MANIPULATION ---------------  */
+/** Combine js files and make sourcemaps */
+gulp.task('concatScripts', function() {
+  return gulp.src(paths.scripts)
+      .pipe(maps.init())
+      .pipe(concat('global.js'))
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('js'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+/** Rename sctipt files to reflect  impending minification */
+gulp.task('renameScripts', ['concatScripts'], function() {
+  return gulp.src('./js/*.js')                          
+      .pipe(rename('all.min.js'))
+      .pipe(gulp.dest(dist.scripts));
+});
+
+/** Minify script files and store in final destination */
+gulp.task('scripts', ['lint', 'cleanScripts', 'concatScripts'], function() {
+  return gulp.src('./js/*.js')                          
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(gulp.dest(dist.scripts));
+});
+
+/** IMAGE MANIPULATION -------------------  */
+/** Optimize images and output them in dist */
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+    .pipe(imagemin())
+    .pipe(gulp.dest(dist.content));
+});
+
+/** BUILD TASKS ------------------------  */
+gulp.task("prebuild", ['clean', 'concatScripts', 'compileSass', 'images'], function() {
+    return gulp.src(["icons/**"], { base: './'})
+        .pipe(gulp.dest('dist')); 
+});
+
+gulp.task("build", ['prebuild'], function() {
+    gulp.src('./*.html')
+        .pipe(useref())
+        .pipe(iff('*.js', uglify()))         
+        .pipe(iff('*.css', csso()))
+        .pipe(gulp.dest('./dist')); 
+});
+
+/** SERVE TASK -----------------------  */
+gulp.task('serve', ['watchFiles']);
+
+/** DEFAULT TASKS --------------------  */
+gulp.task("default", function() {
+  gulp.start('build');
+});
